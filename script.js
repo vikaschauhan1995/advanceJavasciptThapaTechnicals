95d4f765b4cfd5880cad8abacd7a75345fa4640e
Vikas Chauhan <vikas@rockstarchef.in>
2022-10-23 13:59:17 +0530

    function and variable hoisting

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
-function a() {
-  const fa = "this is funciton a start";
-  console.log(fa);
-  b();
-  console.log("function a end");
+
+sum(5, 10);
+function sum(a, b) {
+  add = a + b;
+  console.log(add);
 }
-function b() {
-  console.log("this is function b");
-  c();
+// sum2(10, 20); // Uncaught TypeError: sum2 is not a function
+var sum2 = function (a, b) {
+  add = a + b;
+  console.log(add);
 }
-function c() {
-  console.log("this is c function");
+
+// sum3(20, 10); //Uncaught ReferenceError: Cannot access 'sum3' before initialization
+const sum3 = (a, b) => {
+  add = a + b;
+  console.log(add);
 }
-a();
+
+console.log(myName);
+var myName = "vikas";
+
+
+a = 10;
+console.log(a);
+var a;
+
+var b = 20;
+console.log(b);
+var b = 40;
+
+// var c;
+console.log("c=", c);
+var c = 50;
+
+
+console.log("I am a " + job); // I am a undefined
+var job = "Web Developer";
+console.log("I am a " + job); // I am a Web Developer
+
+function whoAmI() {
+  console.log("I am a " + job); // I am a undefined
+  var job = "Full stack";
+  console.log("I am a " + job); // I am a Full Stack
+}
+whoAmI();

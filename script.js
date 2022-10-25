8b06db00a283d37c5f67908e2fadc4f8f3af0d39
Vikas Chauhan <vikas@rockstarchef.in>
2022-10-25 20:27:45 +0530

    this keyword in javascirpt

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-// "use strict"
 
-function sum(a, b) {
-  // add = a + b; // Uncaught ReferenceError: add is not defined
-  var add = a + b;
-  console.log(add);
+function sum() {
+  let add = 10 + 10;
+  console.log(this);
 }
-sum(10, 20);
+// sum();
 
-// function sub(a, a) { // Duplicate parameter name not allowed in this
-function sub(a, a) {
-  subs = a - 20;
-  console.log(subs);
+const vikas = {
+  fullName: 'Vikas Chauhan',
+  sum() {
+    console.log(this); // ! this keyword points the vikas object
+  },
+  min: () => {
+    // ! Inside a arrow function "this" points the window object
+    console.log(this); // ! this keyword points to window object
+  },
+  calculation: function () {
+    console.log("calculation", this); // ! this keyword points vikas object
+    /*
+    * @this key
+    */
+    function child() {
+      console.log("child", this);// ! this keyword points to window object
+    }
+    child();
+    let child2 = () => {
+      console.log("child2", this);// ! inside arrow function "this" points vikas object
+    }
+    child2();
+  }
 }
-sub(40, 10);
+vikas.sum();
+vikas.min();
+vikas.calculation();

0de42af69199301ce877599d20a71a47707aa0fa
Vikas Chauhan <vikas@rockstarchef.in>
2022-10-26 22:10:50 +0530

    array is an object

--- script.js
+++ script.js
@@ -1,33 +1,12 @@
-let a = 1;
-let b = "vikas";
-let c = true;
-let d;
-let e = "";
-let f = null;
+const arr1 = [1, 2, 3];
 
-console.log(typeof (c));
-console.log(typeof (d));
-console.log(typeof (e));
-console.log(typeof (f))
-console.log("-----------------------------");
-const person = {
-  name: "vikas",
+const arrObj1 = {
+  0: 1,
+  1: 2,
+  2: 3
 };
-person.age = 27;
-console.log(typeof (person));
-console.log(typeof ({}));
-console.log(typeof ([]));
 
+console.log(typeof (arr1));
+console.log(typeof (arrObj1));
 
-const book = () => {
-  console.log("this is book function");
-}
-book.thickness = "250pages";
-console.log(book.thickness);
-
-
-
-console.log("---------------------------------");
-const car = ["tata", "mahindra"];
-car._2 = "toyoya";
-console.log(car);
+console.log(Array.isArray(arr1));

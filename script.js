efa05aedbe6dfec6e07df8a03c0763914a2c8b0c
Vikas Chauhan <vikas@rockstarchef.in>
2022-11-05 22:53:03 +0530

    destructuring of array and object in javascript

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-var a = 5, b = 10;
+let [fName, lName] = ["Vikas", "Chauhan"];
 
-[b, a] = [a, b]
+console.log(fName, lName);
 
-// a = b, b = a;
-console.log(a, b);
-// swap or interchange two values without using third variable
+const person1 = {
+  firstName: "Anuj",
+  lastName: "Chauhan"
+};
+var { f_name, l_name } = person1; // variable name and property name must be same
+console.log(f_name, l_name); // output: undefined undefined
+var { firstName, lastName } = person1;
+console.log(firstName, lastName); // Anuj Chauhan
+var { firstName: myFirstName, lastName: myLastName } = person1;
+console.log(myFirstName, myLastName); // Anuj Chauhan
+
+const person2 = {
+  name: "Yogesh",
+  work: {
+    languages: {
+      spoken: "English",
+      computer: ".net"
+    }
+  }
+};
+const {
+  name: n,
+  work: {
+    languages,
+    languages: { spoken, computer }
+  }
+} = person2;
+console.log(`name = ${n}
+work = ${spoken}
+languages = ${languages, spoken}
+`);

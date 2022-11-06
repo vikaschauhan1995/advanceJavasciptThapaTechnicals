a61c3bf592106c590048d58a1ebefe18c2973f10
Vikas Chauhan <vikas@rockstarchef.in>
2022-11-06 14:12:05 +0530

    default parameter

--- script.js
+++ script.js
@@ -1,35 +1,19 @@
-let [fName, lName] = ["Vikas", "Chauhan"];
+let a = true ? 5 : 0;
+console.log(a);
 
-console.log(fName, lName);
+// default parameters
+function multiply(a, b) {
+  b = (b === undefined) ? 1 : b;
+  console.log("multiply = ", a * b);
+}
+multiply(4);
 
-const person1 = {
-  firstName: "Anuj",
-  lastName: "Chauhan"
-};
-var { f_name, l_name } = person1; // variable name and property name must be same
-console.log(f_name, l_name); // output: undefined undefined
-var { firstName, lastName } = person1;
-console.log(firstName, lastName); // Anuj Chauhan
-var { firstName: myFirstName, lastName: myLastName } = person1;
-console.log(myFirstName, myLastName); // Anuj Chauhan
+function addition(a, b = 1) {
+  console.log("addition = ", a + b);
+}
+addition(4);
 
-const person2 = {
-  name: "Yogesh",
-  work: {
-    languages: {
-      spoken: "English",
-      computer: ".net"
-    }
-  }
-};
-const {
-  name: n,
-  work: {
-    languages,
-    languages: { spoken, computer }
-  }
-} = person2;
-console.log(`name = ${n}
-work = ${spoken}
-languages = ${languages, spoken}
-`);
+const substract = (a, b = 3) => {
+  console.log(`${a - b}`);
+}
+substract(10);

5958652678f4568233563dbe96e88d300484f38b
Vikas Chauhan <vikas@rockstarchef.in>
2022-11-10 20:03:52 +0530

    Promise, then and catch methods

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 
-function callBackHell() {
-  setTimeout(function () {
-    const roleNumbers = [1, 2, 3, 4, 5];
-    console.log(roleNumbers);
-    setTimeout((roleNumber) => {
-      const obj = {
-        name: 'Vikas',
-        age: 27
-      };
-      console.log(obj);
-      setTimeout((obj) => {
-        const gender = 'male';
-        console.log(`My name is ${obj.name} and i am ${obj.age} years old, gender ${gender}`);
-      }, 2000, obj);
-    }, 2000, roleNumbers[1]);
+
+const rollNumbersPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const numbers = [1, 2, 3, 4, 5, 6];
+    numbers.length > 0 ? resolve(numbers) : reject("Roll numbers no available!")
   }, 2000)
-}
-callBackHell();
+});
+
+const getStudentByRollNumberPromise = (rollNumber) => {
+  return new Promise((resolve, reject) => {
+    setTimeout((rNumber) => {
+      const student = {
+        name: 'Yogesh',
+        rollNumber: rNumber,
+        gender: 'Male'
+      };
+      resolve(student);
+    }, 2000, rollNumber);
+  });
+};
+
+rollNumbersPromise.then((rollNumbers) => {
+  console.log(rollNumbers);
+  return getStudentByRollNumberPromise(rollNumbers[1]);
+}).then((student) => {
+  console.log(student);
+}).catch(error => {
+  console.log("Error: ", error);
+});

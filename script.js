7f4bfc8b6d253b3b4c1e715a67b44c17973b0199
Vikas Chauhan <vikas@rockstarchef.in>
2022-11-10 19:32:47 +0530

    callbackhell

--- script.js
+++ script.js
@@ -1,43 +1,19 @@
-function Person(name) {
-  this.name = name;
-  this.bioData = () => {
-    console.log(`My name is ${this.name}`);
-  }
+
+function callBackHell() {
+  setTimeout(function () {
+    const roleNumbers = [1, 2, 3, 4, 5];
+    console.log(roleNumbers);
+    setTimeout((roleNumber) => {
+      const obj = {
+        name: 'Vikas',
+        age: 27
+      };
+      console.log(obj);
+      setTimeout((obj) => {
+        const gender = 'male';
+        console.log(`My name is ${obj.name} and i am ${obj.age} years old, gender ${gender}`);
+      }, 2000, obj);
+    }, 2000, roleNumbers[1]);
+  }, 2000)
 }
-const person1 = new Person('Anuj');
-const person2 = new Person('Yogesh')
-person1.bioData();
-person2.bioData();
-
-
-class Student {
-  constructor(name) {
-    this.name = name;
-  }
-  bioData() {
-    console.log(`My name is ${this.name}`);
-  }
-}
-const student1 = new Student('Vikas');
-student1.bioData();
-
-class Player extends Student {
-  constructor(name, game) {
-    super(name);
-    this.game = game;
-  }
-  playGame() {
-    console.log(`My name is ${this.name}, I play ${this.game}`);
-  }
-  callParentBio() {
-    return super.bioData();
-  }
-}
-let player1 = new Player('player1 name');
-player1.bioData();
-let player2 = new Player('player2 name', 'ludo');
-player2.playGame();
-console.log("---------");
-player2.callParentBio();
-
-
+callBackHell();
